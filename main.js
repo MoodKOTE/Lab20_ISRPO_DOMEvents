3c62dbea1956603fb0e3be364a94c77569c4a93a
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-04-06 18:07:17 +0300

    Step 6: input and button interaction

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-console.log(document);
+// console.log(document);
 
-const title = document.getElementById("title");
-const text = document.querySelector(".text");
-console.log(title);
-console.log(text);
+// const title = document.getElementById("title");
+// const text = document.querySelector(".text");
+// console.log(title);
+// console.log(text);
 
-title.textContent = "DOM изменён через JS!";
-title.style.color = "blue";
-title.style.backgroundColor = "#f0f0f0";
-title.style.padding = "10px";
+// title.textContent = "DOM изменён через JS!";
+// title.style.color = "blue";
+// title.style.backgroundColor = "#f0f0f0";
+// title.style.padding = "10px";
 
-text.style.fontSize = "18px";
-text.style.fontStyle = "italic"
-text.style.color = "darkgreen";
+// text.style.fontSize = "18px";
+// text.style.fontStyle = "italic"
+// text.style.color = "darkgreen";
 
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-    alert("Кнопка нажата!");
-    btn.style.backgroundColor = "#4CAF50";
-    btn.style.color = "white";
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//     alert("Кнопка нажата!");
+//     btn.style.backgroundColor = "#4CAF50";
+//     btn.style.color = "white";
+// });
+
+const input = document.getElementById("nameInput");
+const button = document.getElementById("showName");
+const output = document.getElementById("output");
+button.addEventListener("click", () => {
+if (input.value.trim() !== ""){
+    output.textContent =`Привет, ${input.value}!`;
+    output.style.color = "green";
+}else{
+    output.textContent =`Пожалуйста, введите имя!`;
+    output.style.color = "red";
+}
 });
+
